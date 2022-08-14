67abc7de97b0781a6106f20db4a1936226363a2f
amreemaru <heartof.effort@gmail.com>
2022-08-14 15:20:17 +0700

    solved 1-3

--- python1/item1_3.py
+++ python1/item1_3.py
@@ -1,20 +1,24 @@
-# from random import randrange
-
-def count_most_voted(lists):
-    most_vote = list[0]
+def count_most_voted(lists, voters):
     vote_set = set(lists)
+    most_vote = ''
 
-    for c in vote_set:
-        if 0 < int(c) < 21:
-            # print(f"{c:2} = {lists.count(c)}")
-            if lists.count(most_vote) < lists.count(c):
-                most_vote = c
+    for e in vote_set:
+        if 0 < int(e) < 21:
+            most_vote = e
+            break
         else:
             return None
 
     for c in vote_set:
+        if 0 < int(c) < 21 and lists.count(most_vote) <= lists.count(c):
+            most_vote = c
+
+    for c in vote_set:
         if most_vote != c and lists.count(most_vote) == lists.count(c):
             return None
+
+    if 1 > int(most_vote) > 20:
+        return None
 
     return most_vote
 
@@ -22,10 +26,8 @@ def count_most_voted(lists):
 print("*** Election ***")
 voters = int(input("Enter a number of voter(s) : "))
 lists = input().split()
-# lists = [randrange(1,20) for i in range(voters)]
-# print(lists)
 
-if count_most_voted(lists) != None:
-    print(count_most_voted(lists))
+if count_most_voted(lists, voters) != None:
+    print(count_most_voted(lists, voters))
 else:
     print("*** No Candidate Wins ***")
