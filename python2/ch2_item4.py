0792ff82ca9dc0663e5b8d9abdb745650ebff2bb
amreemaru <heartof.effort@gmail.com>
2022-08-17 10:02:48 +0700

    Rename

---
+++ python2/ch2_item4.py
@@ -0,0 +1,31 @@
+def numberToBase(n, b):
+    if n == 0:
+        return [0]
+    
+    digits = []
+    
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    
+    digits = [str(i) for i in digits]
+    return ''.join(digits[::-1])
+
+def hbd(age):
+    b = 2
+    while True:
+        if numberToBase(age, b) == '20':
+            age = 20
+            break
+        elif numberToBase(age, b) == '21':
+            age = 21
+            break
+        else:
+            b += 1
+            
+    return f'saimai is just {age}, in base {b}!'
+    ### Enter Your Code Here ###
+
+year = input("Enter year : ")
+
+print(hbd(int(year)))
